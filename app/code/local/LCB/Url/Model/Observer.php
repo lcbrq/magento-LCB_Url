fe354d2eddc41be6b2b45a2c9eebcd2b377a427f
Tomasz Gregorczyk <tomasz@silpion.com.pl>
2025-06-18 02:41:43 +0200

    Redirect old routes to new ones

--- app/code/local/LCB/Url/Model/Observer.php
+++ app/code/local/LCB/Url/Model/Observer.php
@@ -3,8 +3,33 @@
 class LCB_Url_Model_Observer
 {
     /**
+     * @var bool
+     */
+    private $redirectToNewRoute = true;
+
+    /**
+     * Set 301 redirect on old routes
+     *
+     * @since 0.6.0
+     * @return void
+     */
+    public function redirectToNewRoute(Varien_Event_Observer $observer)
+    {
+        if ($this->redirectToNewRoute) {
+            $controller = $observer->getEvent()->getControllerAction();
+            $request = $controller->getRequest();
+            $routePath = trim((string) $request->getPathInfo(), '/');
+
+            if ($matchedRoute = Mage::helper('lcb_url')->matchRoute($routePath)) {
+                $newUrl = Mage::getUrl($matchedRoute);
+                $controller->getResponse()->setRedirect($newUrl, 301)->sendResponse();
+            }
+        }
+    }
+
+    /**
      * Set NOINDEX/NOFOLLOW meta on pages with rewrite
-     * 
+     *
      * @since 0.5.0
      * @return void
      */
